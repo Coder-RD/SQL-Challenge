1b2869d6a5f5237f917eb9bc0fac7bb22a043073
Rohini Datal <rohinidatal0@gmail.com>
2026-09-14 09:18:44 +0530

    Day-50
    🎯 **Day 50 – 100 Days of SQL Challenge!** 💻🔥
    
    Successfully completed **Day 50** of my SQL Challenge by solving the **Contest Leaderboard** problem on HackerRank. 🏆
    
    📌 **Topic:** Contest Leaderboard
    🗄️ **Focus:** SQL Joins, Aggregation, `GROUP BY`, `SUM()`, and `ORDER BY`
    
    This challenge helped me strengthen my understanding of how to analyze contest data and generate leaderboard results using SQL.
    
    📂 **Day 50 Contents:**
    
    * `Contest Leaderboard.sql` — SQL solution
    * `Screenshot 2026-09-14 085728.png` — Solution screenshot
    
    🚀 **50 Days Completed | 50 Days to Go!**
    💡 Consistency + Practice + Problem Solving = Growth!
    
    #100DaysOfSQL #SQLChallenge #Day50 #HackerRank #SQL #Database #CodingChallenge #LearningInPublic

---
+++ Day50-SQL/Contest Leaderboard.sql
@@ -0,0 +1,12 @@
+
+SELECT h.hacker_id, h.name, SUM(x.max_score) AS total_score
+FROM Hackers h
+JOIN (
+    SELECT hacker_id, challenge_id, MAX(score) AS max_score
+    FROM Submissions
+    GROUP BY hacker_id, challenge_id
+) x
+ON h.hacker_id = x.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING SUM(x.max_score) > 0
+ORDER BY total_score DESC, h.hacker_id ASC;
